2e07abbca5d10ca88b5ac359baf6a1c9efbe4485
Chineye <chineye69@gmail.com>
2018-05-22 15:50:09 -0500

    Fixed Spaced Attempt #2
    Followed Kevin's original and tried to fix the spacing to match.

--- src/main/java/codeu/controller/ChatServlet.java
+++ src/main/java/codeu/controller/ChatServlet.java
@@ -1,42 +1,42 @@
     // Copyright 2017 Google Inc.
-    //
-    // Licensed under the Apache License, Version 2.0 (the "License");
-    // you may not use this file except in compliance with the License.
-    // You may obtain a copy of the License at
-    //
-    //    http://www.apache.org/licenses/LICENSE-2.0
-    //
-    // Unless required by applicable law or agreed to in writing, software
-    // distributed under the License is distributed on an "AS IS" BASIS,
-    // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    // See the License for the specific language governing permissions and
-    // limitations under the License.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-    package codeu.controller;
+package codeu.controller;
 
-    import codeu.model.data.Conversation;
-    import codeu.model.data.Message;
-    import codeu.model.data.User;
-    import codeu.model.store.basic.ConversationStore;
-    import codeu.model.store.basic.MessageStore;
-    import codeu.model.store.basic.UserStore;
-    import java.io.IOException;
-    import java.time.Instant;
-    import java.util.List;
-    import java.util.UUID;
-    import javax.servlet.ServletException;
-    import javax.servlet.http.HttpServlet;
-    import javax.servlet.http.HttpServletRequest;
-    import javax.servlet.http.HttpServletResponse;
-    import org.jsoup.Jsoup;
-    import org.jsoup.safety.Cleaner;
-    import org.jsoup.safety.Whitelist;
-    import org.jsoup.nodes.Document;
-    import org.jsoup.nodes.Element;
-    import org.jsoup.parser.Parser;
+import codeu.model.data.Conversation;
+import codeu.model.data.Message;
+import codeu.model.data.User;
+import codeu.model.store.basic.ConversationStore;
+import codeu.model.store.basic.MessageStore;
+import codeu.model.store.basic.UserStore;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Cleaner;
+import org.jsoup.safety.Whitelist;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.parser.Parser;
 
-    /** Servlet class responsible for the chat page. */
-    public class ChatServlet extends HttpServlet {
+/** Servlet class responsible for the chat page. */
+public class ChatServlet extends HttpServlet {
 
     /** Store class that gives access to Conversations. */
     private ConversationStore conversationStore;
@@ -50,45 +50,46 @@
     /** Set up state for handling chat requests. */
     @Override
     public void init() throws ServletException {
-    super.init();
-    setConversationStore(ConversationStore.getInstance());
-    setMessageStore(MessageStore.getInstance());
-    setUserStore(UserStore.getInstance());
+      super.init();
+      setConversationStore(ConversationStore.getInstance());
+      setMessageStore(MessageStore.getInstance());
+      setUserStore(UserStore.getInstance());
     }
 
-    /**
-    * Sets the ConversationStore used by this servlet. This function provides a
-    * common setup method for use by the test framework or the servlet's init()
-    * function.
-    */
-    void setConversationStore(ConversationStore conversationStore) {
+  /**
+   * Sets the ConversationStore used by this servlet. This function provides a
+   * common setup method for use by the test framework or the servlet's init()
+   * function.
+   */
+  void setConversationStore(ConversationStore conversationStore) {
     this.conversationStore = conversationStore;
-    }
+  }
 
-    /**
-    * Sets the MessageStore used by this servlet. This function provides a common
-    * setup method for use by the test framework or the servlet's init() function.
-    */
-    void setMessageStore(MessageStore messageStore) {
+  /**
+   * Sets the MessageStore used by this servlet. This function provides a common
+   * setup method for use by the test framework or the servlet's init() function.
+   */
+  void setMessageStore(MessageStore messageStore) {
     this.messageStore = messageStore;
-    }
+  }
 
-    /**
-    * Sets the UserStore used by this servlet. This function provides a common
-    * setup method for use by the test framework or the servlet's init() function.
-    */
-    void setUserStore(UserStore userStore) {
+  /**
+   * Sets the UserStore used by this servlet. This function provides a common
+   * setup method for use by the test framework or the servlet's init() function.
+   */
+  void setUserStore(UserStore userStore) {
     this.userStore = userStore;
-    }
+  }
 
-    /**
-    * This function fires when a user navigates to the chat page. It gets the
-    * conversation title from the URL, finds the corresponding Conversation, and
-    * fetches the messages in that Conversation. It then forwards to chat.jsp for
-    * rendering.
-    */
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+  /**
+   * This function fires when a user navigates to the chat page. It gets the
+   * conversation title from the URL, finds the corresponding Conversation, and
+   * fetches the messages in that Conversation. It then forwards to chat.jsp for
+   * rendering.
+   */
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws IOException, ServletException {
     String requestUrl = request.getRequestURI();
     String conversationTitle = requestUrl.substring("/chat/".length());
 
@@ -107,9 +108,9 @@
     request.setAttribute("conversation", conversation);
     request.setAttribute("messages", messages);
     request.getRequestDispatcher("/WEB-INF/view/chat.jsp").forward(request, response);
-    }
+  }
 
-    /**
+  /**
     * This function fires when a user submits the form on the chat page. It gets
     * the logged-in username from the session, the conversation title from the URL,
     * and the chat message from the submitted form data. It creates a new Message
@@ -117,14 +118,15 @@
     * page.
     */
     @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+  public void doPost(HttpServletRequest request, HttpServletResponse response) 
+        throws IOException, ServletException {
 
-    String username = (String) request.getSession().getAttribute("user");
-    if (username == null) {
-    	// user is not logged in, don't let them add a message
-    	response.sendRedirect("/login");
-    	return;
-    }
+      String username = (String) request.getSession().getAttribute("user");
+      if (username == null) {
+    	  // user is not logged in, don't let them add a message
+    	 response.sendRedirect("/login");
+    	 return;
+      }
 
     User user = userStore.getUser(username);
     if (user == null) {
@@ -151,13 +153,18 @@
 
   	String cleanedMessageContent = clean(messageContent, Whitelist.simpleText());
 
-  	Message message = new Message(UUID.randomUUID(), conversation.getId(), user.getId(), cleanedMessageContent,
-  			Instant.now());
+    Message message =
+        new Message(
+            UUID.randomUUID(),
+            conversation.getId(),
+            user.getId(),
+            cleanedMessageContent,
+            Instant.now());
 
-  	messageStore.addMessage(message);
+    messageStore.addMessage(message);
 
-  	// redirect to a GET request
-  	response.sendRedirect("/chat/" + conversationTitle);
+    // redirect to a GET request
+    response.sendRedirect("/chat/" + conversationTitle);
   }
 
   /*
@@ -175,4 +182,4 @@
   	return clean.body().html();
   }
 
-  }
+}
