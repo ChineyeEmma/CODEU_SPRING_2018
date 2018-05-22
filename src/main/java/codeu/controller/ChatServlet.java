4e65ff9783429c10e19ee4e3d3064eb064ae04eb
Chineye <chineye69@gmail.com>
2018-05-22 16:00:40 -0500

    Fixed Indents #2
    Fixed indents for ChatServlet.java. Followed Kevin's original.

--- src/main/java/codeu/controller/ChatServlet.java
+++ src/main/java/codeu/controller/ChatServlet.java
@@ -1,4 +1,4 @@
-    // Copyright 2017 Google Inc.
+// Copyright 2017 Google Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -38,14 +38,14 @@ import org.jsoup.parser.Parser;
 /** Servlet class responsible for the chat page. */
 public class ChatServlet extends HttpServlet {
 
-    /** Store class that gives access to Conversations. */
-    private ConversationStore conversationStore;
+  /** Store class that gives access to Conversations. */
+  private ConversationStore conversationStore;
 
-    /** Store class that gives access to Messages. */
-    private MessageStore messageStore;
+  /** Store class that gives access to Messages. */
+  private MessageStore messageStore;
 
-    /** Store class that gives access to Users. */
-    private UserStore userStore;
+  /** Store class that gives access to Users. */
+  private UserStore userStore;
 
     /** Set up state for handling chat requests. */
     @Override
@@ -57,48 +57,46 @@ public class ChatServlet extends HttpServlet {
     }
 
   /**
-   * Sets the ConversationStore used by this servlet. This function provides a
-   * common setup method for use by the test framework or the servlet's init()
-   * function.
+   * Sets the ConversationStore used by this servlet. This function provides a common setup method
+   * for use by the test framework or the servlet's init() function.
    */
   void setConversationStore(ConversationStore conversationStore) {
     this.conversationStore = conversationStore;
   }
 
   /**
-   * Sets the MessageStore used by this servlet. This function provides a common
-   * setup method for use by the test framework or the servlet's init() function.
+   * Sets the MessageStore used by this servlet. This function provides a common setup method for
+   * use by the test framework or the servlet's init() function.
    */
   void setMessageStore(MessageStore messageStore) {
     this.messageStore = messageStore;
   }
 
   /**
-   * Sets the UserStore used by this servlet. This function provides a common
-   * setup method for use by the test framework or the servlet's init() function.
+   * Sets the UserStore used by this servlet. This function provides a common setup method for use
+   * by the test framework or the servlet's init() function.
    */
   void setUserStore(UserStore userStore) {
     this.userStore = userStore;
   }
 
   /**
-   * This function fires when a user navigates to the chat page. It gets the
-   * conversation title from the URL, finds the corresponding Conversation, and
-   * fetches the messages in that Conversation. It then forwards to chat.jsp for
-   * rendering.
+   * This function fires when a user navigates to the chat page. It gets the conversation title from
+   * the URL, finds the corresponding Conversation, and fetches the messages in that Conversation.
+   * It then forwards to chat.jsp for rendering.
    */
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws IOException, ServletException {
     String requestUrl = request.getRequestURI();
     String conversationTitle = requestUrl.substring("/chat/".length());
 
     Conversation conversation = conversationStore.getConversationWithTitle(conversationTitle);
     if (conversation == null) {
-    	// couldn't find conversation, redirect to conversation list
-    	System.out.println("Conversation was null: " + conversationTitle);
-    	response.sendRedirect("/conversations");
-    	return;
+      // couldn't find conversation, redirect to conversation list
+      System.out.println("Conversation was null: " + conversationTitle);
+      response.sendRedirect("/conversations");
+      return;
     }
 
     UUID conversationId = conversation.getId();
